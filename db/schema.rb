85884e3f3f3101f14ec9d69bcdc4dbcd0e1e97f0
edb-c <edb.cofresi@gmail.com>
2018-10-05 11:13:15 -0400

    Working on Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_221213) do
+ActiveRecord::Schema.define(version: 2018_10_05_125707) do
 
   create_table "assignments", force: :cascade do |t|
     t.string "assignment_name"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2018_10_04_221213) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_students_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end
 
   create_table "teachers", force: :cascade do |t|
@@ -50,6 +57,13 @@ ActiveRecord::Schema.define(version: 2018_10_04_221213) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_teachers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
 
 end
