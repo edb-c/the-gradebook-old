3beaec738861f5d2d606037ed057cafa3a0d8fd5
edb-c <edb.cofresi@gmail.com>
2018-10-04 23:39:36 -0400

    worked on nav bar view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     helpers do
 
       def current_user
-        #  @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+        # @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
       end
 
 #Booleanizing the current_user method
