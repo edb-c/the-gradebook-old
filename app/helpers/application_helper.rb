ff06af54584272f8ff881afc0643da9804c2215f
edb-c <edb.cofresi@gmail.com>
2018-11-08 11:27:58 -0500

    Refactoring code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,7 @@ module ApplicationHelper
     puts "ec - In ApplicationHelper"
   end
 
-  def get_student_name
-    puts "ec - In ApplicationHelper - get student name"
-     sLastName = Student.find(@course_detail.student_id).lastname.capitalize
-     sFirstName = Student.find(@course_detail.student_id).firstname.capitalize
-     student_name = sLastName + ", " + sFirstName
-     student_name
-  end
-
-  def get_student_name2(student_id)
+  def get_student_name(student_id)
     puts "ec - In ApplicationHelper - get student name2(student_id)"
      sLastName = Student.find(student_id).lastname.capitalize
      sFirstName = Student.find(student_id).firstname.capitalize
