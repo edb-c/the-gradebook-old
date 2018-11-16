6e08f862a51a5234103113b6e2eea51ec05d8dec
edb-c <edb.cofresi@gmail.com>
2018-11-16 10:45:26 -0500

    Refactor models

--- app/models/course_detail.rb
+++ app/models/course_detail.rb
@@ -3,13 +3,19 @@ class CourseDetail < ApplicationRecord
   belongs_to :course, optional: :true
   belongs_to :teacher, optional: :true
 
+  has_many  :teacher_courses
   has_many  :teachers, through: :teacher_courses
+
+  has_many  :student_courses
   has_many  :students, through: :student_courses
 
   scope :at_risk_students, -> { where("assignment_grade < 75") }
+  
+  scope :best_students, -> { where("assignment_grade > 85") }
 
   validates :assignment_grade, :numericality => { :greater_than_or_equal_to => 0,
                     :less_than_or_equal_to => 100 }
  
 
 end
+
